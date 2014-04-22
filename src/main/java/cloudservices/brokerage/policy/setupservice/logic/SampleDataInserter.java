6101cc4d38a89a08e15c44dad87b942eed63c72d
Arash <Arash@172.30.33.83>
2014-04-21 23:27:04 -0400

    third level added to crawler4jService

--- src/main/java/cloudservices/brokerage/policy/setupservice/logic/SampleDataInserter.java
+++ src/main/java/cloudservices/brokerage/policy/setupservice/logic/SampleDataInserter.java
@@ -26,7 +26,7 @@ import java.util.logging.Logger;
  * @author Arash Khodadadi http://www.arashkhodadadi.com/
  */
 public class SampleDataInserter {
-    
+
     private final static Logger LOGGER = Logger.getLogger(SampleDataInserter.class
             .getName());
     private ServiceDAO serviceDAO;
@@ -35,7 +35,7 @@ public class SampleDataInserter {
     private ServicePropositionDAO servicePropositionDAO;
     private PolicyServiceDAO policyServiceDAO;
     private PolicyPropositionDAO policyPropositionDAO;
-    
+
     public SampleDataInserter() {
         serviceDAO = new ServiceDAO();
         propositionDAO = new PropositionDAO();
@@ -44,7 +44,7 @@ public class SampleDataInserter {
         policyPropositionDAO = new PolicyPropositionDAO();
         servicePropositionDAO = new ServicePropositionDAO();
     }
-    
+
     public boolean insertSampleData() throws DAOException {
         Proposition firstLevel = new Proposition();
         firstLevel.setName("First Level");
@@ -55,11 +55,11 @@ public class SampleDataInserter {
         Proposition thirdLevel = new Proposition();
         thirdLevel.setName("Third Level");
         thirdLevel.setValid(true);
-        
+
         firstLevel.setId((Long) propositionDAO.save(firstLevel));
         secondLevel.setId((Long) propositionDAO.save(secondLevel));
         thirdLevel.setId((Long) propositionDAO.save(thirdLevel));
-        
+
         Proposition crawler4jFinished = new Proposition();
         crawler4jFinished.setName("Crawler4j Finished");
         crawler4jFinished.setValid(true);
@@ -69,42 +69,42 @@ public class SampleDataInserter {
         Proposition websphinxFinished = new Proposition();
         websphinxFinished.setName("Websphinx Finished");
         websphinxFinished.setValid(true);
-        
+
         crawler4jFinished.setId((Long) propositionDAO.save(crawler4jFinished));
         crawler4jFilteredFinished.setId((Long) propositionDAO.save(crawler4jFilteredFinished));
         websphinxFinished.setId((Long) propositionDAO.save(websphinxFinished));
-        
+
         Proposition filterAvailable = new Proposition();
         filterAvailable.setName("Filter Available");
         filterAvailable.setValid(true);
         Proposition seedsAvailable = new Proposition();
         seedsAvailable.setName("Seeds Available");
         seedsAvailable.setValid(true);
-        
+
         filterAvailable.setId((Long) propositionDAO.save(filterAvailable));
         seedsAvailable.setId((Long) propositionDAO.save(seedsAvailable));
-        
+
         Service crawler4j = new Service();
         crawler4j.setName("crawler4j");
         crawler4j.setWSDLURL("http://localhost:8080/crawler4jService/crawler4jWS?wsdl");
         crawler4j.setId((Long) serviceDAO.save(crawler4j));
         crawler4j.setServicesStr(crawler4j.getId().toString());
         serviceDAO.saveOrUpdate(crawler4j);
-        
+
         Service crawler4jFiltered = new Service();
         crawler4jFiltered.setName("crawler4jFiltered");
         crawler4jFiltered.setWSDLURL("http://localhost:8080/crawler4jService/crawler4jWS?wsdl");
         crawler4jFiltered.setId((Long) serviceDAO.save(crawler4jFiltered));
         crawler4jFiltered.setServicesStr(crawler4jFiltered.getId().toString());
         serviceDAO.saveOrUpdate(crawler4jFiltered);
-        
+
         Service websphinx = new Service();
         websphinx.setName("websphinx");
         websphinx.setWSDLURL("http://localhost:8080/websphinxService/WebsphinxWS?wsdl");
         websphinx.setId((Long) serviceDAO.save(websphinx));
         websphinx.setServicesStr(websphinx.getId().toString());
         serviceDAO.saveOrUpdate(websphinx);
-        
+
         ServiceProposition sp = new ServiceProposition();
         sp.addInputToService(websphinx, seedsAvailable);
         servicePropositionDAO.save(sp);
@@ -114,7 +114,7 @@ public class SampleDataInserter {
         sp = new ServiceProposition();
         sp.addOutputToService(websphinx, websphinxFinished);
         servicePropositionDAO.save(sp);
-        
+
         sp = new ServiceProposition();
         sp.addInputToService(crawler4j, firstLevel);
         servicePropositionDAO.save(sp);
@@ -127,7 +127,10 @@ public class SampleDataInserter {
         sp = new ServiceProposition();
         sp.addOutputToService(crawler4j, secondLevel);
         servicePropositionDAO.save(sp);
-        
+        sp = new ServiceProposition();
+        sp.addOutputToService(crawler4j, thirdLevel);
+        servicePropositionDAO.save(sp);
+
         sp = new ServiceProposition();
         sp.addInputToService(crawler4jFiltered, secondLevel);
         servicePropositionDAO.save(sp);
@@ -143,12 +146,12 @@ public class SampleDataInserter {
         sp = new ServiceProposition();
         sp.addOutputToService(crawler4jFiltered, thirdLevel);
         servicePropositionDAO.save(sp);
-        
+
         Policy policy = new Policy();
         policy.setName("Force Filtered Crawling in Level 3");
         policy.setPriority(1);
         policy.setId((Long) policyDAO.save(policy));
-        
+
         PolicyProposition pp = new PolicyProposition();
         pp.addConditionToPolicy(policy, secondLevel);
         policyPropositionDAO.save(pp);
@@ -158,23 +161,23 @@ public class SampleDataInserter {
         pp = new PolicyProposition();
         pp.addConditionToPolicy(policy, seedsAvailable);
         policyPropositionDAO.save(pp);
-        
+
         pp = new PolicyProposition();
         pp.addEventToPolicy(policy, thirdLevel);
         policyPropositionDAO.save(pp);
         pp = new PolicyProposition();
         pp.addEventToPolicy(policy, crawler4jFinished);
         policyPropositionDAO.save(pp);
-        
+
         PolicyService ps = new PolicyService();
         ps.addActionToPolicy(policy, crawler4jFiltered);
         policyServiceDAO.save(ps);
-        
+
         serviceDAO.saveOrUpdate(crawler4j);
         serviceDAO.saveOrUpdate(crawler4jFiltered);
         serviceDAO.saveOrUpdate(websphinx);
         policyDAO.saveOrUpdate(policy);
-        
+
         Service composite = new Service();
         composite.setName("Composite Level 3 Crawler");
         Set<Service> level1 = new HashSet<>();
@@ -187,7 +190,7 @@ public class SampleDataInserter {
         composite.addServiceLevel(level2);
         composite.addServiceLevel(level3);
         composite.setId((Long) serviceDAO.save(composite));
-        
+
         ServiceProposition csp = new ServiceProposition();
         csp.addInputToService(composite, seedsAvailable);
         servicePropositionDAO.save(csp);
@@ -200,7 +203,7 @@ public class SampleDataInserter {
         csp = new ServiceProposition();
         csp.addOutputToService(composite, crawler4jFinished);
         servicePropositionDAO.save(csp);
-        
+
         serviceDAO.saveOrUpdate(composite);
         return insertPolicyServices();
     }
@@ -212,15 +215,15 @@ public class SampleDataInserter {
         setupService.setId((Long) serviceDAO.save(setupService));
         setupService.setServicesStr(setupService.getId().toString());
         serviceDAO.saveOrUpdate(setupService);
-        
+
         Service serviceExecutor = new Service();
         serviceExecutor.setName("Service Executor");
         serviceExecutor.setWSDLURL("http://localhost:8080/ServiceExecutor/ServiceExecutorWS?wsdl");
         serviceExecutor.setId((Long) serviceDAO.save(serviceExecutor));
         serviceExecutor.setServicesStr(serviceExecutor.getId().toString());
         serviceDAO.saveOrUpdate(serviceExecutor);
-        
-                Service policyManager = new Service();
+
+        Service policyManager = new Service();
         policyManager.setName("Policy Manager");
         policyManager.setWSDLURL("http://localhost:8080/PolicyManager/PolicyManagerWS?wsdl");
         policyManager.setId((Long) serviceDAO.save(policyManager));
